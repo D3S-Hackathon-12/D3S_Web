b5d0b1e0574c41f2b532babf1b0f20daf17a62ad
DSMGun <geon0250@gmail.com>
2017-02-05 00:15:50 +0900

    Fixed 숫자버튼.html and add button on index.html

---
+++ button.js
@@ -0,0 +1,35 @@
+$('#btn1').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '1');
+});
+
+$('#btn2').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '2');
+});
+
+$('#btn3').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '3');
+});
+
+$('#btn4').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '4');
+});
+
+$('#btn5').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '5');
+});
+
+$('#btn6').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '6');
+});
+
+$('#btn7').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '7');
+});
+
+$('#btn8').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '8');
+});
+
+$('#btn9').on("click", function () {
+  $('#number').attr('value', $('#number').attr('value') + '9');
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+$('#red_btn').mousedown(function() {
+  $( this ).css("background-color", "#ffb900");
+});
+
+$('#red_btn').mouseup(function() {
+  $( this ).css("background-color", "#fc1a1a");
+});
